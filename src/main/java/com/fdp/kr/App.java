b4431c7f4346e278f50f4f8baa725173cdb53380
msourabh <sourabhmodi20051@gmail.com>
2017-06-29 16:44:40 +0530

    First

---
+++ src/main/java/com/fdp/kr/App.java
@@ -0,0 +1,13 @@
+package com.fdp.kr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
